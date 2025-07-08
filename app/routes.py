da2ab3173444fe3a8bdcbf49f004ebc6c4717ee8
Tom Bini <bini.tom04@gmail.com>
2025-07-08 10:32:52 +0200

    Reponsiveness ok, color change in nav bar ok, burger tab on mobile added

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template
 from app.projects import projects
+from flask import request
 
 def get_project_by_slug(slug):
     for project in projects:
@@ -11,23 +12,23 @@ def get_project_by_slug(slug):
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", request=request)
 
 @app.route("/skills")
 def skills():
-    return render_template("skills.html")
+    return render_template("skills.html", request=request)
 
 @app.route("/projects")
 def projects_page():
-    return render_template("projects.html", projects=projects)
+    return render_template("projects.html", projects=projects, request=request)
 
 @app.route("/music")
 def music():
-    return render_template("music.html")
+    return render_template("music.html", request=request)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", request=request)
 
 @app.route("/projects/<slug>")
 def project_detail(slug):
